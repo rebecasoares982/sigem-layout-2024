059ee3d5a9e55d1eeb5ea1cb016e83e806161ecf
Rebeca Soares <98848463+rebecasoares982@users.noreply.github.com>
2024-06-04 18:09:10 -0300

    paginas internas (menu) parte 3

--- scripts.js
+++ scripts.js
@@ -72,3 +72,31 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+ 
+//FullCalendar - Agenda
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        events: [
+            {
+                title: 'Evento 1',
+                start: '2024-06-01'
+            },
+            {
+                title: 'Evento 2',
+                start: '2024-06-05',
+                end: '2024-06-07'
+            }
+        ],
+        dateClick: function(info) {
+            alert('Data: ' + info.dateStr);
+        }
+    });
+    calendar.render();
+});
